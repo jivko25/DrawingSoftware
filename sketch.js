fdc733feab97780b340bb415d14eaa1f349225dd
jivko25 <jivkou@yahoo.com>
2021-02-25 18:47:35 +0200

    basic drawing

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
-function setup(){
-    createCanvas(400, 400);
+function selector(select){
+    return document.querySelector(select);
 }
 
-function draw(){
-    background(255,0,0);
+function setup(){
+    createCanvas(650, 600);
+    // canvas.parent("#canvas-wrapper");
+    background(255);
 }
+function draw(){
+    // fill(0);
+    // line(20, 30, 40, 50);
+}
+
+function mouseDragged(){
+    let type = null;
+    let size = parseInt(selector("#pen-size").value);
+    let color = selector("#pen-color").value;
+    if(selector("#pen-pencil").checked){
+        type = "pencil";
+    } else if(selector("#pen-brush").checked){
+        type = "brush";
+    } else if(selector("#pen-eraser").checked){
+        color = "#ffffff";
+    }
+    fill(color);
+    stroke(color);
+    if(type == "pencil"){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    } else{
+        ellipse(mouseX, mouseY, size, size);
+    }
+    console.log(color);
+}
+
