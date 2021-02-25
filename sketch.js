11f3d6325f461148936f5e2eefbc23ff155dc35a
jivko25 <jivkou@yahoo.com>
2021-02-25 22:56:11 +0200

    pencil, brush, eraser, import image, save image, clear added

--- sketch.js
+++ sketch.js
@@ -1,19 +1,36 @@
+let input;
+let img;
+var imageIsSelected = false;
+
 function selector(select){
     return document.querySelector(select);
 }
 
 function setup(){
-    createCanvas(650, 600);
+    let canvas = createCanvas(650, 600);
     // canvas.parent("#canvas-wrapper");
     background(255);
+    Resetbutton = createButton('Reset');
+    Resetbutton.mousePressed(resetBG);
+    Savebutton = createButton('Save');
+    Savebutton.mousePressed(savaImage);
+    input = createFileInput(handleFile);
+    if (img) {
+        // background(img);
+        image(img, 0, 0, width, height);
+        
+      }
 }
 function draw(){
-    // fill(0);
-    // line(20, 30, 40, 50);
+    if (img && imageIsSelected == false) {
+        // background(img);
+        image(img, 0, 0, width, height);
+        imageIsSelected = true;
+      }
 }
 
 function mouseDragged(){
-    let type = null;
+    let type;
     let size = parseInt(selector("#pen-size").value);
     let color = selector("#pen-color").value;
     if(selector("#pen-pencil").checked){
@@ -30,6 +47,27 @@ function mouseDragged(){
     } else{
         ellipse(mouseX, mouseY, size, size);
     }
-    console.log(color);
 }
+
+function resetBG() {
+    background(255);
+    img = null;
+    imageIsSelected = false;
+}
+
+function savaImage(){
+    saveCanvas(canvas, "sketch", "png");
+}
+
+function handleFile(file) {
+    print(file);
+    if (file.type === 'image') {
+      img = createImg(file.data, '');
+      img.hide();
+      imageIsSelected = false;
+    } else {
+      img = null;
+    }
+  }
+
 
